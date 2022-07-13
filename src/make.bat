a68496b6b7d9610b3829b0361108a38f421047bc
CC <jeff@chipnetics.com>
2022-07-13 16:01:00 -0600

    added make,bat for windows

---
+++ src/make.bat
@@ -0,0 +1,5 @@
+call v -prod -cc gcc -d prebuilt xerdiff.v -o ../xerdiff
+call v -prod -cc gcc -d prebuilt xerdump.v -o ../xerdump
+call v -prod -cc gcc -d prebuilt xertask.v -o ../xertask
+call v -prod -cc gcc -d prebuilt xerpred.v -o ../xerpred
+call v -prod -cc gcc -d prebuilt xer2json.v -o ../xer2json
